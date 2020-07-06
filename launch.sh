c609f161c2a0ad9fc254569574d373987a7cef09
nadiabykova <66245980+nadiabykova@users.noreply.github.com>
2020-07-06 14:06:40 +0300

    Add files via upload

--- launch.sh
+++ launch.sh
@@ -1,9 +1,11 @@
 mkdir results/
 mkdir joblib/
+mkdir pics/
 jupyter nbconvert --to script shared_functions.ipynb
-jupyter nbconvert --execute --to pdf --no-input --output results/SupplementaryText0_LoadData.pdf catsNdogs_part0_Load_data.ipynb
-jupyter nbconvert --execute --to pdf --no-input --output results/SupplementaryText0_MDS.pdf catsNdogs_part1_MDS.ipynb
-jupyter nbconvert --execute  --ExecutePreprocessor.timeout=10000  --to pdf --no-input --output results/SupplementaryText1.pdf catsNdogs_part2_MW_test.ipynb jupyter nbconvert --execute --ExecutePreprocessor.timeout=100000 --to pdf --no-input --output results/SupplementaryText2.pdf catsNdogs_part3_RF_v2.ipynb
-
-jupyter nbconvert --execute --ExecutePreprocessor.timeout=100000 --to pdf --no-input --output results/SupplementaryText3.pdf catsNdogs_part4_PJC.ipynb
-jupyter nbconvert --execute --ExecutePreprocessor.timeout=10000 --to pdf --no-input --output results/SupplementaryText0_part5.pdf catsNdogs_part5_Test_new_projects.ipynb
+jupyter nbconvert --execute --to pdf --no-input --output results/output_part1.pdf catsNdogs_part0_Load_data.ipynb
+jupyter nbconvert --execute --to pdf --no-input --output results/output_part2.pdf catsNdogs_part1_MDS.ipynb
+jupyter nbconvert --execute  --ExecutePreprocessor.timeout=10000  --to pdf --no-input --output results/output_part2.pdf catsNdogs_part2_MW_test.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=100000 --to pdf --no-input --output results/output_part3.pdf catsNdogs_part3_RF_v2.ipynb
+jupyter nbconvert --execute --to pdf --no-input --output results/output_part3_2.pdf catsNdogs_part3_2_RF_feature_importance.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=100000 --to pdf --no-input --output results/output_part4.pdf catsNdogs_part4_PJC.ipynb
+jupyter nbconvert --execute --ExecutePreprocessor.timeout=10000 --to pdf --no-input --output results/output_part5.pdf catsNdogs_part5_Test_new_projects.ipynb
